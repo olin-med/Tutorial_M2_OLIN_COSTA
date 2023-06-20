37c8e00138a47d51b227ccf922e8de9653de5a21
zoli3579 <olinmedeiros@usp.br>
2023-06-20 12:49:32 -0300

    db

---
+++ SEMANA_07/02_AUT_EST_ENTREGA/Backend/app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+const DBPATH = "curr.db";
+const db = new sqlite3.Database(DBPATH);
+const port = 3000;
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+app.get("/inteli", (req, res) => {
+  const db = new sqlite3.Database(DBPATH, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the database.");
+  });
+  db.all(
+    'SELECT * FROM formacao WHERE instituicao="inteli"',
+    [],
+    (err, rows) => {
+      if (err) {
+        console.error(err.message);
+      }
+      res.json(rows);
+    }
+  );
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Closed the database connection.");
+  });
+});
+app.get("/experiencias", (req, res) => {
+  const db = new sqlite3.Database(DBPATH, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the database.");
+  });
+  db.all("SELECT * FROM experiencias", [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+    res.json(rows);
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Closed the database connection.");
+  });
+});
+
+app.get("/formacao", (req, res) => {
+  const db = new sqlite3.Database(DBPATH, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the database.");
+  });
+  db.all("SELECT * FROM formacao", [], (err, rows) => {
+    if (err) {
+      console.error(err.message);
+    }
+    res.json(rows);
+  });
+
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Closed the database connection.");
+  });
+});
+
